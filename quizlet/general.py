9b980a4fd1b992a36f8c59014de678a1a7db365c
John M. Shea <jshea@ece.ufl.edu>
2021-06-04 16:47:57 -0400

    Add questions for Ch 1 and use new JSON storage method.

--- quizlet/general.py
+++ quizlet/general.py
@@ -8,6 +8,7 @@ def display_quiz (question, answers=[],  randomize=True, multiple=False,
            question['type']=='multiple_choice':
             if question_background=="":
                 question_background="#6F78FF"
+            #print(question_background)
             display_quiz_mc(question['question'], question['answers'],
                             multiple=False, randomize=randomize,
                             question_background=question_background)
@@ -20,6 +21,8 @@ def display_quiz (question, answers=[],  randomize=True, multiple=False,
         else:
             print("Question type", question['type']," not supported yet")
     else:
+        if question_background=="":
+            question_background="#6F78FF"
         display_quiz_mc(question, answers,
                         multiple=multiple, randomize=randomize,
                         question_background=question_background)
