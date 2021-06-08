321029dde680a512fc4ca934f70643a3fc268c4b
John M. Shea <jshea@ece.ufl.edu>
2021-06-08 16:07:45 -0400

    Reorganize code a little bit to help make it more maintainable and prepare for new types of quizzes.

--- quizlet/dynamic.py
+++ quizlet/dynamic.py
@@ -1,6 +1,7 @@
 from IPython.core.display import display, Javascript, HTML
 import string
 import random
+import pkg_resources
 
 
 def display_questions_dynamic (url, num=1_000_000, shuffle_questions=False,
@@ -23,33 +24,18 @@ def display_questions_dynamic (url, num=1_000_000, shuffle_questions=False,
     #display(HTML(mydiv + script))
     #return
 
+    # print(__name__)
+    resource_package = __name__
+    helpers=pkg_resources.resource_string(resource_package, "helpers.js")
+    # print(helpers)
+
+
+
+    script+=helpers.decode("utf-8")
+
     script+='''
     //console.log(element);
-    
-    // Make a random ID
-    function makeid(length) {
-        var result           = [];
-        var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
-        var charactersLength = characters.length;
-        for ( var i = 0; i < length; i++ ) {
-          result.push(characters.charAt(Math.floor(Math.random() *  charactersLength)));
-       }
-       return result.join('');
-    }
-    
-    // Choose a random subset of an array. Can also be used to shuffle the array
-    function getRandomSubarray(arr, size) {
-        var shuffled = arr.slice(0), i = arr.length, temp, index;
-        while (i--) {
-            index = Math.floor((i + 1) * Math.random());
-            temp = shuffled[index];
-            shuffled[index] = shuffled[i];
-            shuffled[i] = temp;
-        }
-        return shuffled.slice(0, size);
-    }
-    
-    
+
     function check() {
         var id=this.id.split('-')[0];
 
@@ -139,57 +125,9 @@ def display_questions_dynamic (url, num=1_000_000, shuffle_questions=False,
         
 
     }
-    
-    function parse (json, mydiv) {
-        //var mydiv=document.getElementById(myid);
-        var shuffle_questions=mydiv.dataset.shufflequestions;
-        var num_questions=mydiv.dataset.numquestions;
-        var shuffle_answers=mydiv.dataset.shuffleanswers;
-        
-        if (num_questions>json.length) {
-            num_questions=json.length;
-        }
-        
-        var questions;
-        if ( (num_questions<json.length) || (shuffle_questions=="True") ) {
-            //console.log(num_questions+","+json.length);
-            questions=getRandomSubarray(json, num_questions);
-        } else {
-            questions=json;
-        }
-    
-        //console.log("SQ: "+shuffle_questions+", NQ: " + num_questions + ", SA: ", shuffle_answers);
-        questions.forEach((qa, index, array) => {
-            //console.log(qa.question); 
 
-            var id = makeid(8);
-            //console.log(id);
-
-
-            // Create Div to contain question and answers
-            var iDiv = document.createElement('div');
-            iDiv.id = 'quizWrap'+id+index;
-            iDiv.style='max-width: 600px; margin: 0 auto;padding-bottom: 30px;';
-            mydiv.appendChild(iDiv);
-            // iDiv.innerHTML=qa.question;
-
-            // Create div to contain question part
-            var qDiv = document.createElement('div');
-            qDiv.id="quizQn"+id+index;
-            qDiv.style='padding: 20px;color: #fafafa;font-size: 20px;border-radius: 10px; background: #6F78FF;';
-            //qDiv.innerHTML=qa.question;
-            qDiv.textContent=qa.question;
-            iDiv.append(qDiv);
-
-            // Create div to contain answer part
-            var aDiv = document.createElement('div');
-            aDiv.id="quizAns"+id+index;
-            aDiv.style="margin: 10px 0; display: grid; grid-template-columns: auto auto;grid-gap: 10px;";
-            iDiv.append(aDiv);
-            
-            var num_correct=0;
-            
-            var shuffled;
+    function make_mc(qa, shuffle_answers, aDiv, id) {
+             var shuffled;
             if (shuffle_answers=="True") {
                 //console.log(shuffle_answers+" read as true");
                 shuffled=getRandomSubarray(qa.answers, qa.answers.length);
@@ -198,6 +136,8 @@ def display_questions_dynamic (url, num=1_000_000, shuffle_questions=False,
                 shuffled=qa.answers;
             }
         
+
+            var num_correct=0;
 
 
             
@@ -230,6 +170,67 @@ def display_questions_dynamic (url, num=1_000_000, shuffle_questions=False,
                 
                 
             });
+
+    return num_correct;
+   
+    }
+    
+    function parse (json, mydiv) {
+    //var mydiv=document.getElementById(myid);
+    var shuffle_questions=mydiv.dataset.shufflequestions;
+    var num_questions=mydiv.dataset.numquestions;
+    var shuffle_answers=mydiv.dataset.shuffleanswers;
+    
+    if (num_questions>json.length) {
+        num_questions=json.length;
+    }
+    
+    var questions;
+    if ( (num_questions<json.length) || (shuffle_questions=="True") ) {
+            //console.log(num_questions+","+json.length);
+            questions=getRandomSubarray(json, num_questions);
+        } else {
+            questions=json;
+        }
+    
+        //console.log("SQ: "+shuffle_questions+", NQ: " + num_questions + ", SA: ", shuffle_answers);
+        
+        // Iterate over questions
+        questions.forEach((qa, index, array) => {
+            //console.log(qa.question); 
+
+            var id = makeid(8);
+            //console.log(id);
+
+
+            // Create Div to contain question and answers
+            var iDiv = document.createElement('div');
+            iDiv.id = 'quizWrap'+id+index;
+            iDiv.style='max-width: 600px; margin: 0 auto;padding-bottom: 30px;';
+            mydiv.appendChild(iDiv);
+            // iDiv.innerHTML=qa.question;
+
+            // Create div to contain question part
+            var qDiv = document.createElement('div');
+            qDiv.id="quizQn"+id+index;
+            qDiv.style='padding: 20px;color: #fafafa;font-size: 20px;border-radius: 10px; background: #6F78FF;';
+            //qDiv.innerHTML=qa.question;
+            qDiv.textContent=qa.question;
+            iDiv.append(qDiv);
+
+            // Create div to contain answer part
+            var aDiv = document.createElement('div');
+            aDiv.id="quizAns"+id+index;
+            aDiv.style="margin: 10px 0; display: grid; grid-template-columns: auto auto;grid-gap: 10px;";
+            iDiv.append(aDiv);
+
+            console.log(qa.type);
+
+            var num_correct;
+    if ((qa.type=="multiple_choice") || (qa.type=="many_choice")){
+       num_correct=make_mc(qa, shuffle_answers, aDiv, id);
+    }
+
 
             //Make div for feedback
             var fb = document.createElement("div");
