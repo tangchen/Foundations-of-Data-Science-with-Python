f432465f44a73a59af572e27033fdb7a3fd3ac35
John M. Shea <jshea@ece.ufl.edu>
2021-09-22 15:06:22 -0400

    Command line tool to rerun all notebooks that have flashcards.

---
+++ fc-update.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+
+import os
+import re
+import json
+
+
+pattern = re.compile("DEFIN")
+defns=[]
+chdefns=[]
+fileroot=""
+
+
+for filename in os.listdir("."):
+
+    if os.path.isdir(filename):
+        #print(filename)
+        fc_path=filename+"/flashcards"
+        if os.path.isdir(fc_path):
+            #print("\t"+"/flashcards")
+            for fc_file in os.listdir(fc_path):
+                #print("\t\t"+fc_file,end="")
+                nb_file=filename+"/"+fc_file.split(".")[0]+'.ipynb'
+                #print(",", nb_file)
+                if os.path.isfile(nb_file):
+                    returned=os.system("jupyter nbconvert --execute --inplace " + 
+                         nb_file)
+                    if returned !=0:
+                        print(returned)
+                    print()
+
+print("Done")
+
+
